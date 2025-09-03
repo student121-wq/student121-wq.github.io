634b0367ef7d3fd719e1b2914db97e3ab3a09a46
student121-wq <3248159051@qq.com>
2025-09-03 22:12:04 +0800

    Site updated: 2025-09-03 22:12:01

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1756906720933";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1756908714744";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
